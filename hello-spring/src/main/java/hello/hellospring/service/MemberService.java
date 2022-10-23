438d3495f6e22ab4c9150bb4b9f218af9be01fef
ENOCH YEOM <hykqo@naver.com>
2022-10-23 23:30:12 +0900

    회원서비스 개발 - 회원가입(중복X), 전체회원 조회, 아이디찾기

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+//서비스는 비즈니스에 의존적으로 설계할것.
+public class MemberService {
+
+    private  final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원x
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).
+                //Optional로 감싸면 ifPresent같은 메서드를 쓸 수 있다.(값이 있다면.)
+            ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long id){
+        return memberRepository.findById(id);
+    }
+
+}
